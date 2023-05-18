157fab11de84f7ce8f9c38c86122501ce04bce58
Aşkın Özgür <askin@askin.ws>
2023-05-19 00:14:40 +0300

    Add tool whatismyip

---
+++ src/pages/whatismyip.js
@@ -0,0 +1,23 @@
+import Layout from "@/components/layout";
+import {useState} from "react"
+import {useEffect} from "react"
+
+export default function Page() {
+    const [resultValue, setResultValue] = useState(null);
+
+    useEffect(() => {
+        fetch('https://ipbul.tk/txt.php')
+            .then((res) => res.text())
+            .then((data) => {
+                setResultValue(data);
+            });
+    }, []);
+
+    return (
+        <Layout>
+            <div className="redTitle">
+                <h1>Your IP is <span className="greenText" id="ipresult">{ resultValue }</span></h1>
+            </div>
+        </Layout>
+    )
+}
