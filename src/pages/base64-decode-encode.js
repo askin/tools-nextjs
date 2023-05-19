5bbfe071e65fe28f13ed2a4dff7192f9b0fd826f
Aşkın Özgür <askin@askin.ws>
2023-05-19 15:42:15 +0300

    Tool base64 decode/encode added

---
+++ src/pages/base64-decode-encode.js
@@ -0,0 +1,49 @@
+import Layout from "@/components/layout";
+import {useState} from "react"
+import Header from "@/components/header";
+
+export default function Page() {
+    const [inputValue, setInputValue] = useState("");
+    const [resultValue, setResultValue] = useState("");
+
+    function decode() {
+        try {
+            setResultValue(atob(inputValue))
+        } catch (err) {
+            setResultValue(err.message)
+        }
+
+    }
+
+    function encode() {
+        setResultValue(btoa(inputValue))
+    }
+
+    return (
+        <Layout>
+            <Header title="Base64 Decoder/Encoder" />
+            <div className="content-full">
+                <form>
+                    <h1>Enter text:</h1>
+                    <textarea cols="100" rows="10" id="dencoder" name="dencoder" value={inputValue}
+                              onChange={e => {
+                                  setInputValue(e.currentTarget.value);
+                              }}
+                    />
+                    <input type="button" className="button" value="Decode" onClick={decode} />
+                    <input type="button" className="button" value="Encode" onClick={encode}/>
+
+                    <h1>Result: </h1>
+                    <textarea cols="100" rows="10" value={resultValue}
+                              readOnly={true}
+                              onChange={e => {
+                                  setResultValue(e.currentTarget.value);
+                              }}
+                    />
+                    <input type="button" className="button" value="Copy to clipboard" onClick={() => navigator.clipboard.writeText(resultValue)}/>
+                </form>
+                <div id="renderAsHtml"></div>
+            </div>
+        </Layout>
+    )
+}
