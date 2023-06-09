f592034183b1ce566f3f22ae8037819399c39ed0
Aşkın Özgür <askin@askin.ws>
2023-06-09 20:51:04 +0300

    Copy code added

--- src/components/HighlighCode.js
+++ src/components/HighlighCode.js
@@ -6,6 +6,10 @@ import 'prismjs/plugins/line-numbers/prism-line-numbers.js'
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 import 'prismjs/components/prism-json.js'
 
+import "prismjs/plugins/toolbar/prism-toolbar.min.css";
+import "prismjs/plugins/toolbar/prism-toolbar.min";
+import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min";
+
 export default function HighlighCode({code, language}) {
     useEffect(() => {
         Prism.highlightAll();
