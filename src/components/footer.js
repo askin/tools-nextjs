6b9c194c807a43b5b93de96a4a6c07f5fbe904f2
Aşkın Özgür <askin@askin.ws>
2023-06-17 00:02:08 +0300

    Update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,8 +5,8 @@ export default function Footer() {
         <footer className="siteFooter">
             <div align="center">
                 <span title={ "Last Build: " + new Date().toISOString() }>© { new Date().getFullYear() }</span>&nbsp;
-                <Link href="https://askin.ws" target="_blank">Aşkın Özgür</Link>&nbsp;
-                Powered by&nbsp;
+                <Link href="https://askin.ws" target="_blank">Aşkın Özgür</Link>
+                &nbsp;|&nbsp;Powered by&nbsp;
                 <a href="https://nextjs.org/" rel="noopener noreferrer" target="_blank">Next.js</a>
             </div>
         </footer>
